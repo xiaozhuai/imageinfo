be1d63524eb2fdd62c0c50f11295c2bf99878e0b
xiaozhuai <798047000@qq.com>
2021-04-01 21:41:19 +0800

    fix linux build

--- imageinfo.hpp
+++ imageinfo.hpp
@@ -18,7 +18,7 @@
 
 template<typename T>
 inline T __ii_swap_endian(T u) {
-    static_assert(CHAR_BIT == 8, "CHAR_BIT != 8");
+    static_assert(sizeof(uint8_t) == 1, "CHAR_BIT != 8");
     union {
         T u;
         uint8_t u8[sizeof(T)];
