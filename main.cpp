ea3eb17722a641f20729a9b0482898f7d8d4b07f
xiaozhuai <798047000@qq.com>
2023-01-28 20:22:10 +0800

    Optimize cmake scripts

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char **argv) {
     if (argc < 2) {
-        printf("Usage: %s <file>...\n", argv[0]);
+        printf("Usage: %s [FILE]...\n", argv[0]);
         return 1;
     }
 
